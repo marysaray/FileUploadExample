142055834b7b18b0a264f062f1518c075bfbf749
marysaray <82470838+marysaray@users.noreply.github.com>
2022-11-25 20:31:26 -0800

    Created photo class.

---
+++ FileUploadExample/Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileUploadExample.Models
+{
+    public class Photo
+    {
+        [Key]
+        public int PhotoId { get; set; }
+
+        public string Title { get; set; }
+
+        public string PhotoUrl { get; set; }
+    }
+}
