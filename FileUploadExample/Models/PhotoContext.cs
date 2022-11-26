c1411f09ccfa3dd58dad020c374c556ee0046253
marysaray <82470838+marysaray@users.noreply.github.com>
2022-11-25 20:32:12 -0800

    Added DbSet for photos.

---
+++ FileUploadExample/Models/PhotoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FileUploadExample.Models
+{
+    public class PhotoContext : DbContext
+    {
+        public PhotoContext(DbContextOptions<PhotoContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
